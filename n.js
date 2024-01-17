818ea5a9f4b64afdd6b0e69c850572bb9ebebc54
jortiga <jortiga@gmail.com>
2024-01-18 11:58:18 +1300

    Update n.js

--- n.js
+++ n.js
@@ -14,76 +14,74 @@ const Types = {
     EndCall: "EndCall",
 }
 
-webSocket.on('request', (req) => {
-    const connection = req.accept();
-
-    connection.on('message', (message) => {
-        setInterval(function () {connection.send('J Hello! Message From Server!!')}, 1000);
-        try {
-            const data = JSON.parse(message.utf8Data);
-            const currentUser = findUser(data.username)
-            const userToReceive = findUser(data.target)
-            console.log(data)
-
-            switch (data.type) {
-                case Types.SignIn:
-                    if (currentUser) {
-                        return
-                    }
-
-                    users.push({username: data.username, conn: connection, password: data.data})
-                    break
-                case Types.StartStreaming :
-                    if (userToReceive) {
-                            sendToConnection(userToReceive.conn, {
-                                type: Types.StartStreaming,
-                                username: currentUser.username,
-                                target: userToReceive.username
-                            })
-                    }
-                    break
-                case Types.Offer :
-                    if (userToReceive) {
+webSocket.on('connection', connection => {
+  connection.on('message', message => {
+    console.log(`Received message => ${message}`)
+    try {
+        const data = JSON.parse(message.utf8Data);
+        const currentUser = findUser(data.username)
+        const userToReceive = findUser(data.target)
+        console.log(data)
+    
+        switch (data.type) {
+            case Types.SignIn:
+                if (currentUser) {
+                    return
+                }
+    
+                users.push({username: data.username, conn: connection, password: data.data})
+                break
+            case Types.StartStreaming :
+                if (userToReceive) {
                         sendToConnection(userToReceive.conn, {
-                            type: Types.Offer, username: data.username, data: data.data
+                            type: Types.StartStreaming,
+                            username: currentUser.username,
+                            target: userToReceive.username
                         })
-                    }
-                    break
-                case Types.Answer :
-                    if (userToReceive) {
-                        sendToConnection(userToReceive.conn, {
-                            type: Types.Answer, username: data.username, data: data.data
-                        })
-                    }
-                    break
-                case Types.IceCandidates:
-                    if (userToReceive) {
-                        sendToConnection(userToReceive.conn, {
-                            type: Types.IceCandidates, username: data.username, data: data.data
-                        })
-                    }
-                    break
-                case Types.EndCall:
-                    if (userToReceive) {
-                        sendToConnection(userToReceive.conn, {
-                            type: Types.EndCall, username: data.username
-                        })
-                    }
-                    break
-            }
-        } catch (e) {
-            console.log(e.message)
+                }
+                break
+            case Types.Offer :
+                if (userToReceive) {
+                    sendToConnection(userToReceive.conn, {
+                        type: Types.Offer, username: data.username, data: data.data
+                    })
+                }
+                break
+            case Types.Answer :
+                if (userToReceive) {
+                    sendToConnection(userToReceive.conn, {
+                        type: Types.Answer, username: data.username, data: data.data
+                    })
+                }
+                break
+            case Types.IceCandidates:
+                if (userToReceive) {
+                    sendToConnection(userToReceive.conn, {
+                        type: Types.IceCandidates, username: data.username, data: data.data
+                    })
+                }
+                break
+            case Types.EndCall:
+                if (userToReceive) {
+                    sendToConnection(userToReceive.conn, {
+                        type: Types.EndCall, username: data.username
+                    })
+                }
+                break
         }
-
-    });
-    connection.on('close', () => {
+    } catch (e) {
+        console.log(e.message)
+    }
+  })
+  setInterval(function () {connection.send('Hello! Message From Server!!')}, 1000);
+  connection.on('close', () => {
         users.forEach(user => {
             if (user.conn === connection) {
                 users.splice(users.indexOf(user), 1)
             }
         })
     })
-});
+})
 
 
 const sendToConnection = (connection, message) => {
