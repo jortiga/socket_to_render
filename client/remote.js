2d9abd35e271b3687c1df118356c39323a3e1a5f
jortiga <jortiga@gmail.com>
2024-01-18 17:10:51 +1300

    works

--- client/remote.js
+++ client/remote.js
@@ -1,4 +1,5 @@
-var serverString = "wss://websocket-90ug.onrender.com"
+var serverString = 'ws://localhost:3000'
+//"wss://websocket-90ug.onrender.com"
 //'ws://4.tcp.ngrok.io:16530'
 //'wss://0d4c-2401-7000-dbed-9401-4bc-3dfc-5e07-b1bc.ngrok-free.app'
 //'ws://localhost:3000';  
@@ -7,9 +8,15 @@ var mediaConstraints = {'mandatory': {'OfferToReceiveVideo':true}};
 
 const remoteVideo = document.getElementById('remoteVideo');
 const webcamVideo = document.getElementById('webcamVideo');
+
 const myid = document.getElementById('myname');
-const targetid = document.getElementById('targetname');
-const callbutton = document.getElementById('callid');
+myid.hidden = true;
+
+const requestCallId = document.getElementById('requestCallId');
+const requestButton = document.getElementById('requestButton');
+
+const shareCallId = document.getElementById('shareCallId');
+const shareButton = document.getElementById('shareButton');
 
 let remoteDS = null;
 let localSD = null;
@@ -18,6 +25,9 @@ var candidates = [];
 //let remoteStream = null;
 let localStream = null;
 let dataChannel = null;
+
+let localUsername;
+let localTargetName;
 
 const socket = new WebSocket(serverString);
 var pc = new RTCPeerConnection({
@@ -45,23 +55,7 @@ dataChannel.onmessage = function (event) {
 // Create a audio or video transceiver
 pc.addTransceiver('video')
 
-
-// // Pull tracks from remote stream, add to video stream
-// pc.ontrack = (event) => {
-
-// 	remoteVideo.srcObject = event.streams[0];
-
-// 	event.streams[0].getTracks().forEach((track) => {
-// 	  console.log(track.id);
-// 	  remoteStream.addTrack(track);
-// 	});	
-
-// //remoteVideo.srcObject = remoteStream;
-// };
-
-
 pc.onicecandidate  = (event) => {
-
 
 	if(event.candidate == null) 
 		return;
@@ -77,66 +71,84 @@ pc.onicecandidate  = (event) => {
 		serverUrl: ""
 	}
 
-	// var data = {
-	// 	type: 'IceCandidates',
-	// 	username: myid.value,
-	// 	target: targetid.value,
-	// 	data: JSON.stringify(ff)
-	// };
-
-	socket.send(JSON.stringify({'data': JSON.stringify(ff), 'type': "IceCandidates", 'username': myid.value, 'target': targetid.value}));
-
-	/*if (remoteDS != null)
-	{
-		var rtcIceCandidate = new RTCIceCandidate(event.candidate)	
-    	pc.addIceCandidate(rtcIceCandidate);
-
-    	console.log("got it adding" + event.candidate);
-	}
-	else
-		candidates.push(event.candidate);*/
-
-	//console.log(JSON.stringify({'data': JSON.stringify(ff), 'type': "IceCandidates", 'username': myid.value, 'target': targetid.value}));
+	var out = JSON.stringify({'data': JSON.stringify(ff), 'type': "IceCandidates", 'username': myid.value, 'target': localTargetName});
+	socket.send(out);
 };
 
-//remoteStream = new MediaStream();
+window.onload = async function() {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true });
+  
+    // Push tracks from local stream to peer connection
+	  localStream.getTracks().forEach((track) => {
+	    pc.addTrack(track, localStream);
+	  });
 
-callbutton.onclick = async () => {
+ 	webcamVideo.srcObject = localStream;
 
-	var calld = {
-		type: 'Offer',
-		username: myid.value,
-		target: targetid.value		
+	// Pull tracks from remote stream, add to video stream
+	// pc.ontrack = (event) => {
+	// 	event.streams.forEach(str => {
+	// 		str.getTracks().forEach((track) => {
+	// 		//remoteStream.addTrack(track);
+	// 		remoteVideo.srcObject = str;
+	// 	})});
+	// };
+
+	pc.ontrack = (event) => {
+
+		event.streams[0].getTracks().forEach((track) => {
+		//remoteStream.addTrack(track);
+			remoteVideo.srcObject = event.streams[0];
+		});
+
+		remoteVideo.autoplay = true;
+		remoteVideo.playsInline = true;
+		remoteVideo.muted = true;
 	};
 
+	const labelContainer = document.getElementById('labelContainer');
+
+	// Create a new label element
+	const labelElement = document.createElement('label');
+
+	// Set the label's text content based on a value from JavaScript
+	const labelText = generateRandomAlphanumeric(2); // Replace with your desired label text
+	labelElement.textContent = labelText;
+	myid.value = labelText;
+
+	localUsername = myid.value;
+	// Append the label to the container
+	labelContainer.appendChild(labelElement);
+};
+
+
+shareButton.onclick = async () => {
+
+	var calld = {
+		username: myid.value,
+		target: shareCallId.value		
+	};
+
+	localTargetName = shareCallId.value;
 	call (calld)
 };
 
+requestButton.onclick = async () => {
 
-webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true });
-  
+	var calld = {
+		type: 'StartStreaming',
+		username: myid.value,
+		target: requestCallId.value		
+	};
 
-  // Push tracks from local stream to peer connection
-  localStream.getTracks().forEach((track) => {
-    pc.addTrack(track, localStream);
-  });
+	localTargetName = requestCallId.value;
+	var oo = JSON.stringify(calld);
+	socket.send(oo);
+};
 
-  // Pull tracks from remote stream, add to video stream
-  pc.ontrack = (event) => {
-  	
-    event.streams[0].getTracks().forEach((track) => {
-      //remoteStream.addTrack(track);
-      remoteVideo.srcObject = event.streams[0];
-    });
-
-    remoteVideo.autoplay = true;
-	remoteVideo.playsInline = true;
-	remoteVideo.muted = true;
-  };
-
-    webcamVideo.srcObject = localStream;
-
+// Event handler for when the connection is established
+socket.addEventListener('open', (event) => {
+	console.log('Signing In ' + myid.value);
 
 	var signinData = {
 		type: 'SignIn',
@@ -145,13 +157,6 @@ webcamButton.onclick = async () => {
 	};
 
 	socket.send(JSON.stringify(signinData));
-};
-
-
-
-// Event handler for when the connection is established
-socket.addEventListener('open', (event) => {
-	//console.log('WebSocket connection is open.');
 });
 
 // Event handler for receiving messages from the server
@@ -159,37 +164,23 @@ socket.addEventListener('message', (event) => {
 	console.log('Received from server:', event);
 	var message = JSON.parse(event.data);
 
-
 	switch (message.type) {
 		case "StartStreaming":
-			call(message);		
+			localTargetName = message.username;
+			var calld = {				
+				username: myid.value,
+				target: message.username		
+			};
+			call(calld);		
 			break;
 		case "EndCall":
 		// Code to execute if expression matches caseValue2
 		break;
 		case "Offer":
-			offer(message);
-            
+			OnReceiveOffer(message);            
 		break;
 		case "Answer":
-		    var remoteDescription = message.data;
-			
-	        var rtcSessionDescription = new RTCSessionDescription({
-		        type: 'answer', // or 'offer' depending on the message type
-		        sdp: remoteDescription
-		    });
-
-
-    		remoteDS = remoteDescription;
-		    pc.setRemoteDescription(rtcSessionDescription);
-
-			candidates.forEach((candidate) => {
-		  		var rtcIceCandidate = new RTCIceCandidate(event.candidate)	
-    			pc.addIceCandidate(rtcIceCandidate);
-			});
-
-			candidates = [];
-
+		    OnReceiveAnswer(message)
 		break;
 		case "IceCandidates":
 			var candidateData = JSON.parse(message.data);
@@ -219,7 +210,30 @@ socket.addEventListener('close', (event) => {
 });
 
 
-async function offer(message)
+async function OnReceiveAnswer(message)
+{
+	var remoteDescription = message.data;
+			
+    var rtcSessionDescription = new RTCSessionDescription({
+        type: 'answer', // or 'offer' depending on the message type
+        sdp: remoteDescription
+    });
+
+	remoteDS = remoteDescription;
+
+    await pc.setRemoteDescription(rtcSessionDescription);
+
+	candidates.forEach((candidate) => {
+  		var rtcIceCandidate = new RTCIceCandidate(event.candidate)	
+		pc.addIceCandidate(rtcIceCandidate);
+	});
+
+	candidates = [];
+
+	console.log((localSD == null) + " " + (remoteDS == null))
+
+}
+async function OnReceiveOffer(message)
 {
     var remoteDescription = message.data;
 	
@@ -242,27 +256,48 @@ async function offer(message)
 	          });
 	socket.send(rr);
 
+	console.log(rr);
 	localSD = answerDescription.sdp; 
+
+	candidates.forEach((candidate) => {
+  		var rtcIceCandidate = new RTCIceCandidate(event.candidate)	
+		pc.addIceCandidate(rtcIceCandidate);
+	});
+
+	console.log((localSD == null) + " " + (remoteDS == null))
+	candidates = [];
 }
 
 async function call(body) {
 
 	if (localSD != null)
 		return;
-
+	
  	var offerDescription = await pc.createOffer();
   	await pc.setLocalDescription(offerDescription);
 
   	var oo = JSON.stringify({
-	                "target": body.username,
-	                "username": body.target,
+	                "target": body.target,
+	                "username": body.username,
 	                "type": "Offer",
 	                "data": offerDescription.sdp
 	          });
 	socket.send(oo);
 
+	console.log(body);
 	localSD = offerDescription.sdp;
 
 }
 
+function generateRandomAlphanumeric(length) {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  const charsetLength = charset.length;
 
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charsetLength);
+    result += charset.charAt(randomIndex);
+  }
+
+  return result;
+}
